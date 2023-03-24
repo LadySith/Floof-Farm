585825cb1145871221ea5d939270a2c8b63ad1db
Kigyo <kigyodev@gmail.com>
2023-03-24 14:23:45 +0100

    Inventory system & UI Display

--- FloofFarm/Assets/Scripts/Player.cs
+++ FloofFarm/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int itemsHeld;
+    public List<Collectible> itemsHeld = new List<Collectible>();
 
     private void Update()
     {
