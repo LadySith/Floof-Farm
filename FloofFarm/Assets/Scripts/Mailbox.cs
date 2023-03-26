b59ade66eca62ec3b94a6b756fc47c8d2b9b9a14
4threset <4threset@gmail.com>
2023-03-26 23:53:15 +0800

    Updated Mailbox to trigger a Mail Bubble after 5f.
    (It doesn't do anything yet)

--- FloofFarm/Assets/Scripts/Mailbox.cs
+++ FloofFarm/Assets/Scripts/Mailbox.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Mailbox : MonoBehaviour
 {
     public Player player;
+    public SpriteRenderer mail;
 
+
+    bool deliveredBunny = false;
+    bool deliveredCat = false;
+    bool deliveredFox = false;
 
     // Update is called once per frame
     void OnMouseDown()
@@ -15,23 +20,44 @@ public class Mailbox : MonoBehaviour
         {
             print("Delivered Bunny With CottonBall!");
             player.itemsHeld.RemoveAt(player.selectedItem);
+            deliveredBunny = true;
+            StartCoroutine(postCardDelivery());
         }
 
         if (player.itemsHeld.Count > 0 && player.itemsHeld[player.selectedItem] && player.itemsHeld[player.selectedItem].floofType == FloofType.CATSPJS && (GameManager.instance.mousePosition - player.transform.position).magnitude < player.actionRadius)
         {
             print("Delivered Cat In PJs!");
             player.itemsHeld.RemoveAt(player.selectedItem);
+            deliveredCat = true;
+            StartCoroutine(postCardDelivery());
         }
 
         if (player.itemsHeld.Count > 0 && player.itemsHeld[player.selectedItem] && player.itemsHeld[player.selectedItem].floofType == FloofType.FOXTAIL && (GameManager.instance.mousePosition - player.transform.position).magnitude < player.actionRadius)
         {
             print("Delivered Fox With Fluffy Tail!");
             player.itemsHeld.RemoveAt(player.selectedItem);
+            deliveredFox = true;
+            StartCoroutine(postCardDelivery());
         }
 
 
     }
 
-        
+
+    IEnumerator postCardDelivery()
+    {
+        if (deliveredBunny == true || deliveredCat == true || deliveredFox == true)
+        {
+            yield return new WaitForSeconds(5f);
+            mail.enabled = true;
+            print("Postcard!");
+        }
+
+        else
+        {
+            mail.enabled = false;
+        }
+    }
+
     
 }
